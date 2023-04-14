72521c1ba6ab239e2c4424414bbb0807e694c7de
Евгений Поляков <delpedro@mail.ru>
2023-04-14 22:34:25 +0300

    add redux and load data from API to components Home, Product, Categories

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,50 +1,48 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { API_URL } from "../../utils"
-import axios from "axios"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_URL } from "../../utils";
+import axios from "axios";
 
 export const getProducts = createAsyncThunk(
-    'products/getProducts',
-    async (_, thunkAPI) => {
-        try{
-            const responce = await axios.get(`${API_URL}/products`)
-            return responce.data
-
-        }catch(e){
-            console.log(err)
-            return thunkAPI.rejectWithValue(err)
-        }
+  "products/getProducts",
+  async (_, thunkAPI) => {
+    try {
+      const responce = await axios.get(`${API_URL}/products`);
+      return responce.data;
+    } catch (e) {
+      console.log(err);
+      return thunkAPI.rejectWithValue(err);
     }
-)
-
+  }
+);
 
 const productsSlice = createSlice({
-    name: 'products',
-    initialState: {
-        list: [],
-        filtered: [],
+  name: "products",
+  initialState: {
+    list: [],
+    filtered: [],
+  },
+  // related: [],
+  isLoading: false,
+  reducers: {
+    filrerByPrice: (state, { payload }) => {
+      state.filtered = state.list.filter(({ price }) => price < payload);
     },
-    // related: [],
-    isLoading: false,
-    reducers: {
-        filrerByPrice: (state, { payload }) => {
-            state.filtered = state.list.filter(({ price }) => price < payload)
-        } 
-    },
-    extraReducers: (builder) => {
-        builder.addCase(getProducts.pending, (state, {payload}) =>{
-            state.isLoading = true
-            state.list = payload
-        })
-        builder.addCase(getProducts.fulfilled, (state, {payload}) =>{
-            state.list = payload
-            state.isLoading = false
-        })
-        builder.addCase(getProducts.rejected, (state, {payload}) =>{
-            state.isLoading = false
-            console.log('Ошибка ответа от API')
-        })
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getProducts.pending, (state, { payload }) => {
+      state.isLoading = true;
+      state.list = payload;
+    });
+    builder.addCase(getProducts.fulfilled, (state, { payload }) => {
+      state.list = payload;
+      state.isLoading = false;
+    });
+    builder.addCase(getProducts.rejected, (state, { payload }) => {
+      state.isLoading = false;
+      console.log("Ошибка ответа от API");
+    });
+  },
+});
 
 export const { filrerByPrice } = productsSlice.actions;
 
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,49 +1,42 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { API_URL } from "../../utils"
-import axios from "axios"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_URL } from "../../utils";
+import axios from "axios";
 
-export const getuser = createAsyncThunk(
-    'user/getuser',
-    async (_, thunkAPI) => {
-        try{
-            const responce = await axios.get(`${API_URL}/user`)
-            return responce.data
-
-        }catch(e){
-            console.log(err)
-            return thunkAPI.rejectWithValue(err)
-        }
-    }
-)
-
+export const getuser = createAsyncThunk("user/getuser", async (_, thunkAPI) => {
+  try {
+    const responce = await axios.get(`${API_URL}/user`);
+    return responce.data;
+  } catch (e) {
+    console.log(err);
+    return thunkAPI.rejectWithValue(err);
+  }
+});
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState: {
-        currentUser: [],
-        cart: []
-        // filtered: [],
-    },
-    // related: [],
-    isLoading: false,
-    reducers: {
-      
-    },
-    extraReducers: (builder) => {
-        // builder.addCase(getuser.pending, (state, {payload}) =>{
-        //     state.isLoading = true
-        //     state.list = payload
-        // })
-        // builder.addCase(getuser.fulfilled, (state, {payload}) =>{
-        //     state.list = payload
-        //     state.isLoading = false
-        // })
-        // builder.addCase(getuser.rejected, (state, {payload}) =>{
-        //     state.isLoading = false
-        //     console.log('Ошибка ответа от API')
-        // })
-    }
-})
+  name: "user",
+  initialState: {
+    currentUser: [],
+    cart: [],
+    // filtered: [],
+  },
+  // related: [],
+  isLoading: false,
+  reducers: {},
+  extraReducers: (builder) => {
+    // builder.addCase(getuser.pending, (state, {payload}) =>{
+    //     state.isLoading = true
+    //     state.list = payload
+    // })
+    // builder.addCase(getuser.fulfilled, (state, {payload}) =>{
+    //     state.list = payload
+    //     state.isLoading = false
+    // })
+    // builder.addCase(getuser.rejected, (state, {payload}) =>{
+    //     state.isLoading = false
+    //     console.log('Ошибка ответа от API')
+    // })
+  },
+});
 
 // export const { filrerByPrice } = userSlice.actions;
 
