26fa8af52095137c8dfba6514b74aa4493727fd5
Евгений Поляков <delpedro@mail.ru>
2023-04-16 21:05:17 +0300

    create UI input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,88 +9,97 @@ import { toggleForm } from "../../features/user/userSlice";
 import { useGetProductsQuery } from "../../features/api/apiSlice";
 
 const Header = () => {
-  const {cart, favourites} = useSelector(({user})=> user)
-  const getCount = (arr) => arr.reduce((sum, item)=> sum + item.quantity, 0)
-  const { currentUser } = useSelector(({user}) => user)
-  const dispatch = useDispatch()
-  
-  const [values, setValues] = useState({name: 'Guest', avatar: AVATAR})
-  useEffect(()=>{
-    if(!currentUser) return
-    setValues(currentUser)
-  },[currentUser])
+  const { cart, favourites } = useSelector(({ user }) => user);
+  const getCount = (arr) => arr.reduce((sum, item) => sum + item.quantity, 0);
+  const { currentUser } = useSelector(({ user }) => user);
+  const dispatch = useDispatch();
 
-  const handleClick =()=>{
-    if(!currentUser) dispatch(toggleForm(true))
-  }
+  const [values, setValues] = useState({ name: "Guest", avatar: AVATAR });
+  useEffect(() => {
+    if (!currentUser) return;
+    setValues(currentUser);
+  }, [currentUser]);
 
-  const [searchValue, setSearchValue] = useState('')
-  const { data, isLoading } = useGetProductsQuery({title: searchValue})
-  console.log( data )
+  const handleClick = () => {
+    if (!currentUser) dispatch(toggleForm(true));
+  };
+
+  const [searchValue, setSearchValue] = useState("");
+  const { data, isLoading } = useGetProductsQuery({ title: searchValue });
+  console.log(data);
 
   return (
-    <>    <div className={classes.header}>
-      <div className={classes.logo}>
-        <Link to="/">
-          <img src={LOGOIMG} alt="Stuff"></img>
-        </Link>
-      </div>
-
-      <div className={classes.info}>
-        <div className={classes.user} onClick={handleClick}>
-          <div
-            className={classes.avatar}
-            style={{ backgroundImage: `url(${values.avatar})` }}
-          />
-          <div className={classes.username}> {values.name}</div>
-        </div>
-
-        <form className={classes.form__search}>
-          <svg className={classes.icon}>
-            <use xlinkHref={`${SPRITE}#search`} />
-          </svg>
-          <input
-            placeholder="Search ..."
-            className={classes.input}
-            autoComplete="off"
-            value={searchValue}
-            onChange={({target}) => setSearchValue(target.value) }
-          />
-          <div className="box"></div>
-
-          {searchValue && <div className={classes.box}>
-          { isLoading ? 'Loading' : !data.length ? 'No results' : 
-          (
-            data.map(({title, images, id}) => (
-              <Link className={classes.item} key={id} to={`products/${id}`} >
-              <div className={classes.image} style={{backgroundImage: `url(${images[0]}})`}}></div>
-              <div className={classes.title}>{title}</div>
-              </Link>
-
-            ))
-          )}
-          </div>}
-
-        </form>
-
-        <div className={classes.account}>
-          <Link to="/" className={classes.favourites}>
-            <svg className={classes["icon-fav"]}>
-              <use xlinkHref={`${SPRITE}#heart`} />
-            </svg>
-            <span className={classes.count}></span>
-          </Link>
-          <Link to="/" className={classes.cart}>
-            <svg className={classes["icon-fav"]}>
-              <use xlinkHref={`${SPRITE}#bag`} />
-            </svg>
-            <span className={classes.count}>{ getCount(cart) }</span>
+    <>
+      {" "}
+      <div className={classes.header}>
+        <div className={classes.logo}>
+          <Link to="/">
+            <img src={LOGOIMG} alt="Stuff"></img>
           </Link>
         </div>
+
+        <div className={classes.info}>
+          <div className={classes.user} onClick={handleClick}>
+            <div
+              className={classes.avatar}
+              style={{ backgroundImage: `url(${values.avatar})` }}
+            />
+            <div className={classes.username}> {values.name}</div>
+          </div>
+
+          <form className={classes.form__search}>
+            <svg className={classes.icon}>
+              <use xlinkHref={`${SPRITE}#search`} />
+            </svg>
+            <input
+              placeholder="Search ..."
+              className={classes.input}
+              autoComplete="off"
+              value={searchValue}
+              onChange={({ target }) => setSearchValue(target.value)}
+            />
+            <div className="box"></div>
+
+            {searchValue && (
+              <div className={classes.box}>
+                {isLoading
+                  ? "Loading"
+                  : !data.length
+                  ? "No results"
+                  : data.map(({ title, images, id }) => (
+                      <Link
+                        className={classes.item}
+                        key={id}
+                        to={`products/${id}`}
+                      >
+                        <div
+                          className={classes.image}
+                          style={{ backgroundImage: `url(${images[0]}})` }}
+                        ></div>
+                        <div className={classes.title}>{title}</div>
+                      </Link>
+                    ))}
+              </div>
+            )}
+          </form>
+
+          <div className={classes.account}>
+            <Link to="/" className={classes.favourites}>
+              <svg className={classes["icon-fav"]}>
+                <use xlinkHref={`${SPRITE}#heart`} />
+              </svg>
+              <span className={classes.count}></span>
+            </Link>
+            <Link to="/" className={classes.cart}>
+              <svg className={classes["icon-fav"]}>
+                <use xlinkHref={`${SPRITE}#bag`} />
+              </svg>
+              <span className={classes.count}>{getCount(cart)}</span>
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
     </>
-
   );
 };
 
